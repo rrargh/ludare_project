76cf49231d62b17fd2ac033ce90486cb554052bc
Arg Galinato <rrargh@yahoo.com>
2018-12-02 23:11:53 -0800

    Updated tests.py and decorators.py

--- ludare_project/ludare_app/decorators.py
+++ ludare_project/ludare_app/decorators.py
@@ -5,7 +5,7 @@ from rest_framework.views import status
 def validate_request_data(fn):
     def decorated(*args, **kwargs):
         state = args[0].request.data.get("state", "")
-        due_date = args[0].request.data.get("due_date", "")
+        due_date = args[0].request.data.get("due_date", None)
         text = args[0].request.data.get("text", "")
         if not state and not due_date and not text:
             return Response(
--- ludare_project/ludare_app/tests.py
+++ ludare_project/ludare_app/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+import json
+from datetime import datetime
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
@@ -8,6 +9,28 @@ from .serializers import TodosSerializer
 
 # Create your tests here.
 
+# tests for models
+
+
+class TodosModelTest(APITestCase):
+    def setUp(self):
+        self.my_todo = Todos.objects.create(
+            state="T",
+            due_date=datetime.now(),
+            text="Call Mom"
+        )
+
+    def test_song(self):
+        """"
+        This test ensures that the todo created in the setup
+        exists
+        """
+        self.assertEqual(self.my_todo.state, "T")
+        self.assertEqual(self.my_todo.due_date, datetime.now())
+        self.assertEqual(self.my_todo.text, "Call Mom")
+        self.assertEqual(str(self.my_todo), "Call Mom")
+
+# tests for views
 
 class BaseViewTest(APITestCase):
     client = APIClient()
@@ -21,16 +44,82 @@ class BaseViewTest(APITestCase):
                     text=text
             )
 
+    def make_a_request(self, kind="post", **kwargs):
+        """
+        Make a post request to create a todo
+        :param kind: HTTP VERB
+        :return:
+        """
+        if kind == "post":
+            return self.client.post(
+                reverse(
+                    "todos-list-create",
+                    kwargs={
+                        "version": kwargs["version"]
+                    }
+                ),
+                data=json.dumps(kwargs["data"]),
+                content_type='application/json'
+            )
+        elif kind == "put":
+            return self.client.put(
+                reverse(
+                    "todos-detail",
+                    kwargs={
+                        "version": kwargs["version"],
+                        "pk": kwargs["id"]
+                    }
+                ),
+                data=json.dumps(kwargs["data"]),
+                content_type='application/json'
+            )
+        else:
+            return None
+
+    def fetch_a_todo(self, pk=0):
+        return self.client.get(
+            reverse(
+                "todos-detail",
+                kwargs={
+                    "version": "v1",
+                    "pk": pk
+                }
+            )
+        )
+
+    def delete_a_todo(self, pk=0):
+        return self.client.delete(
+            reverse(
+                "todos-detail",
+                kwargs={
+                    "version": "v1",
+                    "pk": pk
+                }
+            )
+        )
+
     def setUp(self):
         # add test data
-        self.create_todo("todo", "2018-12-25", "buy eggs")
-        self.create_todo("in-progress", "2018-12-25", "buy milk")
-        self.create_todo("done", "2018-12-25", "buy cheese")
+        self.create_todo("T", datetime.now(), "buy eggs")
+        self.create_todo("I", datetime.now(), "buy milk")
+        self.create_todo("D", datetime.now(), "buy cheese")
+        self.valid_data = {
+            "state": "T",
+            "due_date": datetime.now(),
+            "text": "test text"
+        }
+        self.invalid_data = {
+            "state": "",
+            "due_date": None,
+            "text": ""
+        }
+        self.valid_todo_id = 1
+        self.invalid_todo_id = 100
 
 
 class GetAllTodosTest(BaseViewTest):
 
-    def test_got_all_todos(self):
+    def test_get_all_todos(self):
         """
         This test ensures that all todos added in the setUp method
         exist when we make a GET request to the todos/ endpoint
@@ -44,3 +133,98 @@ class GetAllTodosTest(BaseViewTest):
         serialized = TodosSerializer(expected, many=True)
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetASingleTodosTest(BaseViewTest):
+
+    def test_get_a_todo(self):
+        """
+        This test ensures that a single todo of a given id is
+        returned
+        """
+        # hit the API endpoint
+        response = self.fetch_a_todo(self.valid_todo_id)
+        # fetch the data from db
+        expected = Todos.objects.get(pk=self.valid_todo_id)
+        serialized = TodosSerializer(expected)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # test with a todo that does not exist
+        response = self.fetch_a_todo(self.invalid_todo_id)
+        self.assertEqual(
+            response.data["message"],
+            "TODO with ID: 100 does not exist"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class AddTodosTest(BaseViewTest):
+
+    def test_create_a_todo(self):
+        """
+        This test ensures that a single todo can be added
+        """
+        # hit the API endpoint
+        response = self.make_a_request(
+            kind="post",
+            version="v1",
+            data=self.valid_data
+        )
+        self.assertEqual(response.data, self.valid_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # test with invalid data
+        response = self.make_a_request(
+            kind="post",
+            version="v1",
+            data=self.invalid_data
+        )
+        self.assertEqual(
+            response.data["message"],
+            "TODO item requires state, due_date and text"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateTodosTest(BaseViewTest):
+
+    def test_update_a_todo(self):
+        """
+        This test ensures that a single todo can be updated. In this
+        test we update the second todo in the db with valid data and
+        the third todo with invalid data and make assertions
+        """
+        # hit the API endpoint
+        response = self.make_a_request(
+            kind="put",
+            version="v1",
+            id=2,
+            data=self.valid_data
+        )
+        self.assertEqual(response.data, self.valid_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # test with invalid data
+        response = self.make_a_request(
+            kind="put",
+            version="v1",
+            id=3,
+            data=self.invalid_data
+        )
+        self.assertEqual(
+            response.data["message"],
+            "TODO item requires state, due_date and text"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteTodosTest(BaseViewTest):
+
+    def test_delete_a_todo(self):
+        """
+        This test ensures that a todo of given id can be deleted
+        """
+        # hit the API endpoint
+        response = self.delete_a_todo(1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # test with invalid data
+        response = self.delete_a_todo(100)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
